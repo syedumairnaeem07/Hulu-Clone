1cbf905e01a927af23b0934a026c3b2a1e2321f1
umair <sumairn.1997@gmail.com>
2020-10-13 18:46:29 +0500

    Completed Nav-Bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Nav from "./Nav";
+import Results from "./Results";
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
-			{/* Header */}
-
-			{/* Nav bar */}
-
+			<Nav />
+			<Results />
 			{/* Results */}
 		</div>
 	);
---
+++ src/Results.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Results.css"
+
+function Results() {
+	return (
+		<div className="results">
+			<h1>Results here!!!1</h1>
+		</div>
+	);
+}
+
+export default Results;
